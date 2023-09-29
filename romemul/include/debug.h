b3cb0c63a072e7ac8a1014249af4fed05572293e
Diego Parrilla <diego.parrilla.santamaria@gmail.com>
2023-09-29 12:14:59 +0200

    New DPRINT and DPRINTRAW debug print

---
+++ romemul/include/debug.h
@@ -0,0 +1,28 @@
+#ifndef DEBUG_H
+#define DEBUG_H
+
+/**
+ * @brief A macro to print debug
+ *
+ * @param fmt The format string for the debug message, similar to printf.
+ * @param ... Variadic arguments corresponding to the format specifiers in the fmt parameter.
+ */
+#ifdef _DEBUG
+#define DPRINTF(fmt, ...)                                                                  \
+    do                                                                                     \
+    {                                                                                      \
+        const char *file = strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__; \
+        fprintf(stderr, "%s:%d:%s(): " fmt "", file,                                       \
+                __LINE__, __func__, ##__VA_ARGS__);                                        \
+    } while (0)
+#define DPRINTFRAW(fmt, ...)                 \
+    do                                       \
+    {                                        \
+        fprintf(stderr, fmt, ##__VA_ARGS__); \
+    } while (0)
+
+#else
+#define DPRINTF(fmt, ...)
+#endif
+
+#endif // DEBUG_H
