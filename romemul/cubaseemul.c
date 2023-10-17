13a8256e7723a616219a411d9560a493da7737ed
Diego Parrilla <diego.parrilla.santamaria@gmail.com>
2023-10-17 16:30:08 +0200

    missing base file

---
+++ romemul/cubaseemul.c
@@ -0,0 +1,10 @@
+/**
+ * File: cubaseemul.c
+ * Author: Diego Parrilla Santamaría
+ * Date: August 2023
+ * Copyright: 2023 - GOODDATA LABS SL
+ * Description: Cubase dongle emulation experiment
+ */
+
+#include "include/cubaseemul.h"
+
